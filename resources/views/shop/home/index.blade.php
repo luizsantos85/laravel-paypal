593bd5c9a785b87d5746f29a25cfd0f6c4c03524
Luiz Henrique Santos <luiz.santos85@gmail.com>
2022-04-29 21:51:24 -0300

    configuracoes iniciais

---
+++ resources/views/shop/home/index.blade.php
@@ -0,0 +1,17 @@
+@extends('shop.layouts.app')
+
+@section('title', 'Home')
+@section('content')
+
+<section class="contianer">
+    <h1><i class="bi bi-shop"></i>Home da Shop</h1>
+</section>
+
+
+@endsection
+
+{{-- @push('scripts')
+<script>
+    alert('Oi')
+</script>
+@endpush --}}
